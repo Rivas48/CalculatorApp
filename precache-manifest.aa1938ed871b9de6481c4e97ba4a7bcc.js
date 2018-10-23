fbcd2c51e4a0f1ae4a2ea41dca1519363bef7c95
Bryant Rivas <bryantrivas1127@yahoo.com>
2018-10-23 00:25:18 -0400

    Updates

---
+++ precache-manifest.aa1938ed871b9de6481c4e97ba4a7bcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "946b35b7ff05fc354526",
+    "url": "/CalculatorApp/static/js/runtime~main.946b35b7.js"
+  },
+  {
+    "revision": "60fb8e768dd0b2a79b32",
+    "url": "/CalculatorApp/static/js/main.60fb8e76.chunk.js"
+  },
+  {
+    "revision": "4d54b8938665b2f3a0d3",
+    "url": "/CalculatorApp/static/js/1.4d54b893.chunk.js"
+  },
+  {
+    "revision": "60fb8e768dd0b2a79b32",
+    "url": "/CalculatorApp/static/css/main.9bd6d5e3.chunk.css"
+  },
+  {
+    "revision": "e50bc8becab7fd45b0deb833940f561b",
+    "url": "/CalculatorApp/index.html"
+  }
+];
